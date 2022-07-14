2fd28a66252428248596c26bcde8811043def2f4
Joel <9orangetigers@gmail.com>
2022-07-13 20:16:26 -0700

    Complete my custom enumerables

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,74 @@
 module Enumerable
-  # Your code goes here
+  def my_each_with_index
+    i = 0
+    while i < self.length
+      yield(self[i], i)
+      i += 1
+    end
+    self
+  end
+
+  def my_select
+    selected = []
+    self.my_each do |i|
+      if yield(i)
+        selected.push(i)
+      end
+    end
+    selected
+  end
+
+  def my_all?
+    self.my_each do |i|
+      return false unless yield(i)
+    end
+    true
+  end
+
+  def my_any?
+    self.my_each do |i|
+      return true if yield(i)
+    end
+    false
+  end
+
+  def my_none?
+    self.my_each do |i|
+      return false if yield(i)
+    end
+    true
+  end
+
+  def my_count(item = nil)
+    count = 0
+    if item != nil
+      self.my_each do |i|
+        count += 1 if i == item
+      end
+    elsif block_given?
+      self.my_each do |i|
+        count += 1 if yield(i)
+      end
+    else
+      count = self.length
+    end
+    count
+  end
+
+  def my_map
+    mapped = []
+    self.my_each do |i|
+      mapped.push(yield(i))
+    end
+    mapped
+  end
+
+  def my_inject(acc)
+    self.my_each do |i|
+      acc = yield(acc, i)
+    end
+    acc
+  end
 end
 
 # You will first have to define my_each
@@ -7,5 +76,12 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    i = 0
+    while i < self.length
+      yield(self[i])
+      i += 1
+    end
+    self
+  end
 end
